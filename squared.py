768ec7498af43f617d78e8ad66c5a2071ce3fc9f
rvslinger999 <55197989+rvslinger999@users.noreply.github.com>
2019-09-11 10:35:05 -0400

    i wuz here

--- squared.py
+++ squared.py
@@ -6,3 +6,5 @@ mynum_int = int(mynum) # cast string to integer for math expression
 mynum_output = mynum_int * mynum_int # assign value of math expression to new variable
 mynum_str = str(mynum_output) # cast integer to a string for output using print() function
 print("Your answer is " + mynum_str) # output answer to screen using print()
+
+--ryan wuz here--
